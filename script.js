f55f2e3d1e7fe5925a365678a1b21326e176eeb4
AdaHep <adam.hepsever@gmail.com>
2021-12-03 15:14:02 +0100

    Kompletto (for now)

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const adam = document.getElementById('me');
+//Funktion för att kunna trycka på bilden på första sidan för att komma vidare
 
 window.addEventListener('click', main);
 
 function main() {
-    adam.onclick = redirect();
+    const adam = document.getElementById('me');
+    adam.addEventListener('click', redirect)
 }
 
 function redirect (){
